7a364585deee7553fce78a592ecf75e5fbe980b1
alejandro platero rebollo <alejandroplatero29@gmail.com>
2019-10-12 15:41:44 +0200

    Finished Ejercicio3

--- Practicas_PHP/Ejercicio3.php
+++ Practicas_PHP/Ejercicio3.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio 2</title>
+    <title>Ejercicio 3</title>
     <style type="text/css">
         h1 { text-align: center; }
     </style>
     <?php
     /*Ejercicio 3.Modifique el ejercicio anterior para que muestre al lado de cada cuadrado si es un número par o impar.  */
+    $potencia;
+    $texto;
     ?>
 </head>
 <body>
     <h1>Ejercicio 3</h1>
     <?php
     for ($i=1; $i <= 10; $i++) { 
-    ?>
-        <h3><?php echo $i ?> ² =  <?php echo pow($i,2) ?> </h3>
+        $potencia = pow($i,2);
+        if ($potencia % 2 == 0) $texto = "Es par";
+        else $texto = "Es impar";
+        ?>
+        <h3><?php echo $i ?> ² =  <?php echo $potencia ." ". $texto ?></h3>
     <?php }
     ?>
 </body>
