777da5492301b4019ff2d3a3c8f85d3b3b71350b
12alexander21 <alejandroplatero29@gmail.com>
2019-10-14 19:07:27 +0200

    Added Ejercicio1

---
+++ Practicas_JavaScript/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,10 @@
+"use-strict";
+document.write("<table>");
+document.write("<tr><th>TABLA DE MULTIPLICAR DEL 7 </th><th> TABLA DE MULTIPLICAR DEL 8 </th><th> TABLA DE MULTIPLICAR DEL 9</th></tr>")
+
+/*for (let i = 1; i <= 10; i+) {
+    document.write(<t);
+    
+}*/
+
+document.write("</table>");
