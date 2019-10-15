ca4b1debd898c912f2d8d8fcde118be7ba1de3e1
12alexander21 <alejandroplatero29@gmail.com>
2019-10-15 19:54:12 +0200

    Finished Ejercicio1

--- Practicas_JavaScript/Ejercicio1/Ejercicio1.js
+++ Practicas_JavaScript/Ejercicio1/Ejercicio1.js
@@ -1,15 +1,25 @@
 "use-strict";
 
 document.write("<table>");
-document.write("<tr>")
+document.write("<tr>");
 document.write("<th>TABLA DE MULTIPLICAR DEL 7</th>");
 document.write("<th>TABLA DE MULTIPLICAR DEL 8</th>");
 document.write("<th>TABLA DE MULTIPLICAR DEL 9</th>");
+document.write("<th></th>");
 document.write("</tr>");
 document.write("");
-/*for (let i = 1; i <= 10; i+) {
-    document.write(<t);
-    
-}*/
+
+
+for (let i = 1; i <= 10; i++) {
+    siete = 7;
+    ocho = 8;
+    nueve = 9;
+    document.write("<tr>");
+    document.write("<td> 7 x " + i + " = " + i * siete + "</td>");
+    document.write("<td> 8 x " + i + " = " + i * ocho + "</td>");
+    document.write("<td> 9 x " + i + " = " + i * nueve + "</td>")
+    document.write("</tr>");
+}
+
 
 document.write("</table>");
