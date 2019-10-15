c64e0feba8f8245c9593e47c100b32989a686563
12alexander21 <alejandroplatero29@gmail.com>
2019-10-15 16:50:35 +0200

    Refactored Ejercicio1

--- Practicas_JavaScript/Ejercicio1/Ejercicio1.js
+++ Practicas_JavaScript/Ejercicio1/Ejercicio1.js
@@ -1,7 +1,12 @@
 "use-strict";
-document.write("<table>");
-document.write("<tr><th>TABLA DE MULTIPLICAR DEL 7 </th><th> TABLA DE MULTIPLICAR DEL 8 </th><th> TABLA DE MULTIPLICAR DEL 9</th></tr>")
 
+document.write("<table>");
+document.write("<tr>")
+document.write("<th>TABLA DE MULTIPLICAR DEL 7</th>");
+document.write("<th>TABLA DE MULTIPLICAR DEL 8</th>");
+document.write("<th>TABLA DE MULTIPLICAR DEL 9</th>");
+document.write("</tr>");
+document.write("");
 /*for (let i = 1; i <= 10; i+) {
     document.write(<t);
     
