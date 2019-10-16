31a3c29f2f5bd2efeb7b7cc51507b45585c556f3
12alexander21 <alejandroplatero29@gmail.com>
2019-10-16 19:56:15 +0200

    Created Ejercicio18

--- Practicas_PHP/Ejercicio18.php
+++ Practicas_PHP/Ejercicio18.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Ejercicio 18</title>
+    <?php
+        $vector = [["Frutas", "Manzana", "Naranja", "Sandia", "Fresa"], ["Deportes", "Futbol", "Tenis", "Baloncesto", "Beisbol"], ["Idiomas", "Español", "Ingles", "Frances", "Italiano"]];
+    ?>
 </head>
 <body>
     <table>
-        
+        <?php 
+            foreach ($variable as list($titulo, $att1, $att2, $att3, $att4)) {
+            
+        }?>
     </table>
 </body>
 </html>
