ab3a1689471236a39b53446f939391a7427da3dd
12alexander21 <alejandroplatero29@gmail.com>
2019-10-16 20:02:03 +0200

    Modified Ejercicio4

--- Practicas_PHP/Ejercicio4.php
+++ Practicas_PHP/Ejercicio4.php
@@ -13,14 +13,14 @@
 </head>
 <body>
     <?php
-    echo "<h1>Ejercicio 4</h1>";
-    while ($i <= 20) {
-        $resultado *= $i;
-        if ($i != 20) echo "<b id='numeros'>$i x </b>";//Si el número es distinto a 20 añadimos un x 
-        else echo "<b id='numeros'>$i</b>"; //Si no, imprimimos solo el número 
-        $i++;
-    }
-    echo "<b id='resultadoFinal'> = $resultado </b>";
+        echo "<h1>Ejercicio 4</h1>";
+        while ($i <= 20) {
+            $resultado *= $i;
+            if ($i != 20) echo "<b id='numeros'>$i x </b>";//Si el número es distinto a 20 añadimos un x 
+            else echo "<b id='numeros'>$i</b>"; //Si no, imprimimos solo el número 
+            $i++;
+        }
+        echo "<b id='resultadoFinal'> = $resultado </b>";
     ?>
 </body>
 </html>
