c810c6850408e657c1e934666f79efbe08e5e453
12alexander21 <alejandroplatero29@gmail.com>
2019-10-16 17:53:10 +0200

    Added comments

--- Practicas_JavaScript/Ejercicio5/Ejercicio5.js
+++ Practicas_JavaScript/Ejercicio5/Ejercicio5.js
@@ -2,9 +2,9 @@
 
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-let numDNI;
-let letraDNI;
-let resulResto;
+let numDNI; //Variable donde almacenaremos el numero del DNI
+let letraDNI; //Variable donde almacenaremos la letra del DNI
+let resulResto; //Variable para almacenar la letra calculada con la operacion
 numDNI = prompt("Introduzca el DNI sin la letra")
 
 if (numDNI > 0 || numDNI < 99999999){
