40fa6ca071e8cf56ea6c81f5035d6083999b7d4e
12alexander21 <alejandroplatero29@gmail.com>
2019-10-16 19:05:28 +0200

    Added Ejercicio8

---
+++ Practicas_JavaScript/Ejercicio8/Ejercicio8.js
@@ -0,0 +1,26 @@
+"use-strict";
+
+let nota = Number(prompt("Introduzca el número con la que realizar la operación"));
+let result = "";
+
+switch (nota) {
+    case 0: case 1: case 2:
+        result = "MD";
+        break;
+    case 3: case 4:
+        result = "INS";
+        break;
+    case 5:
+        result = "SUF";
+        break;
+    case 6:
+        result = "BIE";
+        break;
+    case 7: case 8:
+        result = "NOT";
+        break;
+    case 9: case 10:
+        result = "SOB";
+        break;
+}
+alert(result);
