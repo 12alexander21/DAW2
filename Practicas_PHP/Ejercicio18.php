b382d28b629023a08f934bf650aba75d2b7a01ae
alejandro platero rebollo <alejandroplatero29@gmail.com>
2019-10-17 10:38:08 +0200

    Modified Ejercicio18

--- Practicas_PHP/Ejercicio18.php
+++ Practicas_PHP/Ejercicio18.php
@@ -5,14 +5,50 @@
     <title>Ejercicio 18</title>
     <?php
         $vector = [["Frutas", "Manzana", "Naranja", "Sandia", "Fresa"], ["Deportes", "Futbol", "Tenis", "Baloncesto", "Beisbol"], ["Idiomas", "Español", "Ingles", "Frances", "Italiano"]];
+        /*        $vector = ["Frutas" =>["Manzana", "Naranja", "Sandia", "Fresa"],
+                 "Deportes" =>["Futbol", "Tenis", "Baloncesto", "Beisbol"],      "Idiomas" =>["Español", "Ingles", "Frances", "Italiano"]]; */
     ?>
 </head>
 <body>
-    <table>
-        <?php 
-            foreach ($variable as list($titulo, $att1, $att2, $att3, $att4)) {
-            
-        }?>
+    <table border=1>
+        <!-- 
+            for $i=0; $i < count$vector, COUNT_RECURSIVE; $i++ ?>
+                <tr>
+                    <td> echo print($vector[$i,$j]) ?></td>
+                    <td> echo $vector[$i] ?></td>
+                    <td> echo $vector[$i] ?></td>
+                </tr>
+                
+           -->
+            <?php foreach ($vector as list($titulo, $att1, $att2, $att3, $att4)) { ?>
+                    <!-- <tr> -->    
+                        
+                    <!-- </tr> -->
+                    <tr>    
+                        <th><?php echo $titulo ?></th>
+                        <td><?php echo $att1 ?></td>
+                    
+                        <td><?php echo $att2 ?></td>
+                    
+                        <td><?php echo $att3 ?></td>
+                    
+                        <td><?php echo $att4 ?></td>
+                    </tr>
+                    
+                    <!--<tr>    
+                        <th><?php echo $clave ?></th>
+                        <td><?php echo $valor[0] ?></td>
+                    
+                        <td><?php echo $valor[1] ?></td>
+                    
+                        <td><?php echo $valor[2] ?></td>
+                    
+                        <td><?php echo $valor[3] ?></td>
+                    </tr>  -->
+
+        <?php } ?>print"<pre>\n";
+                print_r($vector);
+                print"</pre>\n";?>
     </table>
 </body>
 </html>
