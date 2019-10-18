0dbb8c2cc9d28fca057b1089a448a7f98642edf6
12alexander21 <alejandroplatero29@gmail.com>
2019-10-18 17:00:43 +0200

    Got to work Ejercicio18

--- Practicas_PHP/Ejercicio18.php
+++ Practicas_PHP/Ejercicio18.php
@@ -3,52 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Ejercicio 18</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <?php
-        $vector = [["Frutas", "Manzana", "Naranja", "Sandia", "Fresa"], ["Deportes", "Futbol", "Tenis", "Baloncesto", "Beisbol"], ["Idiomas", "Español", "Ingles", "Frances", "Italiano"]];
-        /*        $vector = ["Frutas" =>["Manzana", "Naranja", "Sandia", "Fresa"],
-                 "Deportes" =>["Futbol", "Tenis", "Baloncesto", "Beisbol"],      "Idiomas" =>["Español", "Ingles", "Frances", "Italiano"]]; */
+        $vector = [["Frutas", "Manzana", "Naranja", "Sandia", "Fresa"], ["Deportes", "Futbol", "Tenis", "Baloncesto", "Beisbol"], ["Idiomas", "Español", "Ingles", "Frances", "Italiano"]];;
     ?>
 </head>
 <body>
     <table border=1>
-        <!-- 
-            for $i=0; $i < count$vector, COUNT_RECURSIVE; $i++ ?>
-                <tr>
-                    <td> echo print($vector[$i,$j]) ?></td>
-                    <td> echo $vector[$i] ?></td>
-                    <td> echo $vector[$i] ?></td>
-                </tr>
-                
-           -->
-            <?php foreach ($vector as list($titulo, $att1, $att2, $att3, $att4)) { ?>
-                    <!-- <tr> -->    
-                        
-                    <!-- </tr> -->
-                    <tr>    
-                        <th><?php echo $titulo ?></th>
-                        <td><?php echo $att1 ?></td>
-                    
-                        <td><?php echo $att2 ?></td>
-                    
-                        <td><?php echo $att3 ?></td>
-                    
-                        <td><?php echo $att4 ?></td>
-                    </tr>
-                    
-                    <!--<tr>    
-                        <th><?php echo $clave ?></th>
-                        <td><?php echo $valor[0] ?></td>
-                    
-                        <td><?php echo $valor[1] ?></td>
-                    
-                        <td><?php echo $valor[2] ?></td>
-                    
-                        <td><?php echo $valor[3] ?></td>
-                    </tr>  -->
+        
+    <?php    for ($i=0; $i < count($vector[0]); $i++){ 
+                //for ($j=0; $j < count($vector); $j++) { ?>
 
-        <?php } ?>print"<pre>\n";
-                print_r($vector);
-                print"</pre>\n";?>
+                    <tr>
+                        <td><?php echo $vector[0][$i] ?></td>
+                        <td><?php echo $vector[1][$i] ?></td>
+                        <td><?php echo $vector[2][$i] ?></td>
+                        
+                    </tr>
+                
+                 
+                
+                <?php } ?>
     </table>
 </body>
 </html>
