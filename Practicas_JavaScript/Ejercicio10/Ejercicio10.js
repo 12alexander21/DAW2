c390a0907348f6b40b0879f6d7fb1fe48eb67973
alejandro platero rebollo <alejandroplatero29@gmail.com>
2019-10-19 00:24:23 +0200

    Done Ejercicio10

--- Practicas_JavaScript/Ejercicio10/Ejercicio10.js
+++ Practicas_JavaScript/Ejercicio10/Ejercicio10.js
@@ -22,6 +22,30 @@ document.write("<h3>Suma de los dos primeros números > " +  numeros[0] + " + " 
 document.write("<h3>Multiplicación de los dos primeros números > " +  numeros[2] + " x " + numeros[3] + " = " + numeros[2] * numeros[3] + "</h3>");
 /************************************************************************************* */
 
-let sumaYProd = 0;
+let sumaYProd = "";
+let sumaTodo = 0;
+let prodTodo = 1;
 
-sumaYProd = prompt("Introduzca 4 numeros")
+sumaYProd = prompt("Introduzca 4 cifras");
+
+document.write("<h2>Suma y producto de todos los números</h2><hr>");
+
+for (let i = 0; i < sumaYProd.length; i++) {
+    sumaTodo += Number(sumaYProd[i]);
+    prodTodo *= sumaYProd[i];
+}
+
+document.write("<h3>Suma de todos los números > " + sumaTodo + "</h3>");
+document.write("<h3>Producto de todos los números > " + prodTodo + "</h3>");
+
+/************************************************************************************* */
+
+let precioArt = 0;
+let cantidad = 0;
+
+precioArt = prompt("Introduzca el precio del artículo");
+cantidad = prompt("Introduzca la cantidad");
+
+document.write("<h2>Cálculo del pago del comprador</h2><hr>");
+
+document.write("<h3>Precio total de todos los artículos > " + (precioArt * cantidad) + "</h3>");
