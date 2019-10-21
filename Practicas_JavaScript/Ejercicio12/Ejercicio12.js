d373bd14d93b584768be18789d1f4e124dc936d3
alejandro platero rebollo <alejandroplatero29@gmail.com>
2019-10-21 13:25:47 +0200

    Added Ejercicio12

---
+++ Practicas_JavaScript/Ejercicio12/Ejercicio12.js
@@ -0,0 +1,20 @@
+"use-strict";
+
+let num1 = 0;
+let num2 = 0;
+
+document.write("<br><h3>Cálculo de notas</h3><hr>");
+
+num1 = prompt("Introduzca el primer número");
+num2 = prompt("Introduzca el segundo número");
+
+if (num1 > num2){
+    document.write("<h4>El primer número es mayor que el segundo</h4>");
+    document.write("<h4>Suma > " + (Number(num1) + Number(num2)) + "</h4>");
+    document.write("<h4>Resta > " + (num1 - num2) + "</h4>");
+}
+else if (num2 > num1){
+    document.write("<h4>El segundo número es mayor que el primero</h4>");
+    document.write("<h4>Multiplicación > " + (num1 * num2) + "</h4>");
+    document.write("<h4>División > " + (num1 / num2) + "</h4>");
+}
