7c6b9b5e83431c229e51accdd7c1e0c80d063203
12alexander21 <alejandroplatero29@gmail.com>
2019-10-24 20:53:21 +0200

    Created Ejercicio16

--- Practicas_PHP/Ejercicio16.php
+++ Practicas_PHP/Ejercicio16.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ejercicio 16</title>
+    <?php
+        $cadena = "";
+        !(isset($_GET["cadena"]))
+    ?>
 </head>
 <body>
-    
+    <h1>Autocompletado de cadena</h1>
 </body>
 </html>
