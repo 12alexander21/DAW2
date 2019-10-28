4c53372a3012435bda6126666a0f93ec0e7d4fb8
12alexander21 <alejandroplatero29@gmail.com>
2019-10-28 19:00:37 +0100

    Added Ejercicio24

---
+++ Practicas_JavaScript/Ejercicio24/Ejercicio24.js
@@ -0,0 +1,30 @@
+"use-strict";
+
+let notas = [];
+let nota = 0;
+let mayoresSiete = 0;
+let menoresSiete = 0;
+
+for (let cadaNota = 0; cadaNota < 10; cadaNota++) {
+    nota = prompt("Nota del alumno " + (cadaNota + 1));
+    if (isNaN(nota) || nota.length == 0){
+        alert("Valor incorrecto");
+    }
+    else if (nota < 0 || nota > 10){
+        alert("La nota introducida debe estar entre 0 y 10");
+    }
+    else{
+        notas.push(nota);
+    }
+}
+
+for (let alumno = 0; alumno < notas.length; alumno++) {
+    if (notas[alumno] >= 7){
+        mayoresSiete++;
+    }
+    else{
+        menoresSiete++;
+    }
+}
+document.write("<p>Número de alumnos con notas superiores o iguales a 7 > <b>" + mayoresSiete + "</b></p>");
+document.write("<p>Número de alumnos con notas inferiores a 7 > <b>" + menoresSiete + "</b></p>");
