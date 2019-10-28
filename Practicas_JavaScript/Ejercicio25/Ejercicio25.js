f4207d09c40e7efe51f71d5e7aca5d4a9f15f1e8
12alexander21 <alejandroplatero29@gmail.com>
2019-10-28 19:00:41 +0100

    Added Ejercicio25

---
+++ Practicas_JavaScript/Ejercicio25/Ejercicio25.js
@@ -0,0 +1,25 @@
+"use-strict";
+
+let alturas = [];
+let altura = 0;
+let promedio = 0;
+
+for (let cadaAltura = 0; cadaAltura < 5; cadaAltura++) {
+    altura = prompt("Altura en cm | Persona Nº " + (cadaAltura + 1));
+    if (isNaN(altura)){
+        alert("Valor incorrecto");
+    }
+    else if (altura < 0 || altura > 250){
+        alert("La altura introducida debe estar entre 0 y 250 centímetros");
+    }
+    else{
+        alturas.push(parseInt(altura));
+    }
+}
+
+for (let valor = 0; valor < alturas.length; valor++) {
+    promedio += alturas[valor];
+}
+promedio /= alturas.length;
+
+document.write("<p>La altura promedio es > <b>" + promedio + "</b> centímetros.</p>");
