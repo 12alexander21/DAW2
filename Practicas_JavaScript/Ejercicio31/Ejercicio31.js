cfc15aea940581edae3daa023197f6cdac583ec8
alejandro platero rebollo <alejandroplatero29@gmail.com>
2019-11-05 11:38:00 +0100

    Added Ejercicio31

---
+++ Practicas_JavaScript/Ejercicio31/Ejercicio31.js
@@ -0,0 +1,30 @@
+"use-strict";
+
+let num = 0;
+let numPorTeclado = 0;
+
+
+
+while (true){
+    numPorTeclado = prompt("Introduzca un número");
+    if (isNaN(numPorTeclado)){
+        alert("Valor incorrecto");
+    }
+    else {
+        numPorTeclado = parseInt(numPorTeclado);
+        if (numPorTeclado === 9999) { break; }
+        num += numPorTeclado;
+    }
+}
+
+if (num > 0){
+    document.write("<p>El valor total del número es mayor de cero</p>");
+}
+else if (num === 0){
+    document.write("<p>El valor total del número es igual a cero</p>")
+}
+else {
+    document.write("<p>El valor total del número es menor de cero</p>")
+}
+
+document.write("<p>Resultado > <b> " + num + " </b></p>");
